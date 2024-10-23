063e1845ffa9e6aa3d4bf4222423d196e9473c20
Robiully127 <rislamsohid@gmail.com>
2024-10-23 14:00:07 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -13,14 +13,14 @@ module.exports.run = async function({ api, event, Users, Threads }) {
   const { threadID } = event;
   const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
   const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
-  const type = (event.author == event.logMessageData.leftParticipantFbId) ? "তোর কতো বড় সাহস আমি আরিয়ান bot থাকতে তুই লিফট নিবি? থাপ্পর চিনো তুই☹️😡 🥱দারা তোরে আবার এট দিবো 🤨" : "বাল পাকনামির কারণে কিক খাইলো🤧";
+  const type = (event.author == event.logMessageData.leftParticipantFbId) ? "তোর কতো বড় সাহস আমি আরিয়ান রবিউল থাকতে তুই লিফট নিবি? থাপ্পর চিনো তুই☹️😡 🥱দারা তোরে আবার এট দিবো 🤨" : "বাল পাকনামির কারণে কিক খাইলো🤧";
   const path = join(__dirname, "Nayan", "leaveGif");
   const gifPath = join(path, `Messenger_creation_D9B5E5F7-A4E1-4870-A8E2-D500072CFFF6.mp4`);
   let msg, formPush;
 
   if (existsSync(path)) mkdirSync(path, { recursive: true });
 
-  typeof data.customLeave === "undefined" ? (msg = "ইস {name} {type} 🤖.") : (msg = data.customLeave);
+  typeof data.customLeave === "undefined" ? (msg = "ইস {name} পালিয়ে কোথায় যাবি ঠিক তো ধরে নিয়ে আইছি 😹🫵 {type} 🤖.") : (msg = data.customLeave);
   msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
 
   if (existsSync(gifPath)) {
